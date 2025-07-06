76e43f4baf409a4afd5779c02664302057821c61
kautharalhashmi <kathiialhashmi@gmail.com>
2025-07-06 08:25:28 +0400

    learnedall

--- _starter_files/13_sessions.php
+++ _starter_files/13_sessions.php
@@ -8,3 +8,51 @@
   Sessions are a way to store information (in variables) to be used across multiple pages.
   Unlike cookies, sessions are stored on the server.
 */
+
+
+
+session_start(); // call it before 
+
+if (isset($_POST['submit'])) {
+  $username = filter_input(
+    INPUT_POST,
+    'username',
+    FILTER_SANITIZE_FULL_SPECIAL_CHARS
+  );
+  $password = $_POST['password'];
+
+  if ($username == 'kauthar' && $password == 'password') {
+   
+    $_SESSION['username'] = $username;
+    
+    header('Location: /extras/dashboard.php'); // navigates to dashboard
+  } else {
+    echo 'Incorrect username or password';
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars(
+    $_SERVER['PHP_SELF']
+  ); ?>" method="POST">
+    <div>
+      <label>Username: </label>
+      <input type="text" name="username">
+    </div>
+    <br>
+    <div>
+      <label>Password: </label>
+      <input type="password" name="password">
+    </div>
+    <br>
+    <input type="submit" name="submit" value="Submit">
+  </form>
+</body>
+</html>
